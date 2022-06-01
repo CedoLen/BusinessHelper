224f3d7b82908ef0522d6ce261d4a599945810b4
Cedox <ps-2004@bk.ru>
2022-06-01 23:13:39 +0500

    Authorization and Registration, Adding pages are designed

--- app/src/main/java/com/example/businesshelper/LoginActivity.kt
+++ app/src/main/java/com/example/businesshelper/LoginActivity.kt
@@ -4,15 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        email=findViewById(R.id.AuthEmailAddress)
+        password=findViewById(R.id.AuthTextPassword)
     }
     fun signInApp(view: View) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        if(!email.text.isNullOrEmpty()&&!password.text.isNullOrEmpty())
+        {
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+        }
+        else
+            Toast.makeText(applicationContext,"Заполните поля.", Toast.LENGTH_SHORT).show()
     }
     fun goToRegistration(view: View) {
         val intent = Intent(this, RegistrationActivity::class.java)
--- app/src/main/java/com/example/businesshelper/RegistrationActivity.kt
+++ app/src/main/java/com/example/businesshelper/RegistrationActivity.kt
@@ -4,14 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var email: EditText
+    private lateinit var password:EditText
+    private  lateinit var repeatPassword:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        email = findViewById(R.id.registEmailAddress)
+        password = findViewById(R.id.registPassword)
+        repeatPassword = findViewById(R.id.registPasswordRepeat)
     }
+
     fun createAccount(view: View) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        if(!email.text.isNullOrEmpty()&&!password.text.isNullOrEmpty()&&!repeatPassword.text.isNullOrEmpty())
+        {
+            if(password.text.toString()==repeatPassword.text.toString())
+            {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            Toast.makeText(applicationContext,"Регистрация прошла успешно.",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+            else
+                Toast.makeText(applicationContext,"Пароли не совподают.",Toast.LENGTH_SHORT).show()
+        }
+        else
+            Toast.makeText(applicationContext,"Заполните поля.",Toast.LENGTH_SHORT).show()
+
     }
 }
