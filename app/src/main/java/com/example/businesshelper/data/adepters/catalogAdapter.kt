fe0849d8117ee691690855811724b8c41bcab1ef
Cedox <ps-2004@bk.ru>
2022-06-03 12:58:36 +0500

    Output catalog

---
+++ app/src/main/java/com/example/businesshelper/data/adepters/catalogAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.businesshelper.data.adepters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.businesshelper.R
+import com.example.businesshelper.data.Counterparty
+import com.example.businesshelper.data.Product
+import java.util.ArrayList
+
+class catalogAdapter(val context: Context, val list: ArrayList<Product>): RecyclerView.Adapter<catalogAdapter.MyVM>() {
+    class MyVM(itemView: View):RecyclerView.ViewHolder(itemView) {
+        val title:TextView = itemView.findViewById(R.id.title_product_VM)
+        val price:TextView = itemView.findViewById(R.id.price_product_VM)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVM {
+        val root = LayoutInflater.from(context).inflate(R.layout.product_card,parent,false)
+        return MyVM(root)
+    }
+
+    override fun onBindViewHolder(holder: catalogAdapter.MyVM, position: Int) {
+        holder.title.text = list[position].title
+        holder.price.text= list[position].price.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
--- app/src/main/java/com/example/businesshelper/fragment/CatalogFragment.kt
+++ app/src/main/java/com/example/businesshelper/fragment/CatalogFragment.kt
@@ -1,15 +1,27 @@
 package com.example.businesshelper.fragment
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.businesshelper.R
+import com.example.businesshelper.data.Counterparty
+import com.example.businesshelper.data.Product
+import com.example.businesshelper.data.adepters.catalogAdapter
+import com.example.businesshelper.data.adepters.counterpartiesAdapter
 import com.example.businesshelper.databinding.FragmentCatalogBinding
 import com.example.businesshelper.fragment.addfragment.AddProductFragment
+import com.google.firebase.database.*
+import java.util.ArrayList
 
 class CatalogFragment:Fragment(R.layout.fragment_catalog) {
+    private lateinit var database: DatabaseReference
+    private lateinit var productList: ArrayList<Product>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,6 +36,31 @@ class CatalogFragment:Fragment(R.layout.fragment_catalog) {
                     .commit()
             }
         }
+
+        database = FirebaseDatabase.getInstance().getReference("catalog")
+
+        val recyclerView: RecyclerView = bind.root.findViewById(R.id.recyclerView_catalog)
+        recyclerView.setHasFixedSize(true)
+
+        productList = arrayListOf<Product>()
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                productList.clear()
+                if(snapshot.exists()){
+                    for(item in snapshot.children)
+                    {
+                        val product = item.getValue(Product::class.java)
+                        productList.add(product!!)
+                    }
+                    recyclerView.adapter = catalogAdapter(requireContext(),productList)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(ContentValues.TAG, "loadPost:onCancelled", error.toException())
+            }
+        })
+
         return bind.root
     }
 }
