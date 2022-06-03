89627db20c9aa68dd356092e13d3a724f7bc7ac6
Cedox <ps-2004@bk.ru>
2022-06-03 07:54:48 +0500

    React on setOnClickListener

--- app/src/main/java/com/example/businesshelper/data/adepters/counterpartiesAdapter.kt
+++ app/src/main/java/com/example/businesshelper/data/adepters/counterpartiesAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.businesshelper.R
 import com.example.businesshelper.data.Counterparty
@@ -12,15 +13,23 @@ import java.util.ArrayList
 
 class counterpartiesAdapter(val context: Context, val list: ArrayList<Counterparty>):
     RecyclerView.Adapter<counterpartiesAdapter.MyVM>() {
-        class MyVM(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+     inner class MyVM(itemView: View):RecyclerView.ViewHolder(itemView) {
             val title: TextView =itemView.findViewById(R.id.title_counter_VM)
             val phone:TextView=itemView.findViewById(R.id.phone_counter_VM)
             val email:TextView=itemView.findViewById(R.id.email_counter_VM)
+        init {
+            itemView.setOnClickListener{ v:View->
+                val position:Int=adapterPosition
+                Toast.makeText(itemView.context,"click on item $position",Toast.LENGTH_SHORT).show()
+            }
+
         }
+     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVM {
         val root = LayoutInflater.from(context).inflate(R.layout.counterparty_card,parent,false)
-        return counterpartiesAdapter.MyVM(root)
+        return MyVM(root)
     }
 
     override fun onBindViewHolder(holder: MyVM, position: Int) {
